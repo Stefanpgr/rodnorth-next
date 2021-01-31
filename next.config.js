f45240401a2588cfba26dc52db4d7a56a4c756da
Daniel Agoziem <stefanpongrz@gmail.com>
2021-01-31 08:54:19 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// const withSass = require('@zeit/next-sass')
+// const withImages = require('next-images')
+// const withLess = require('@zeit/next-less')
+// const withCSS = require('@zeit/next-css')
+
+// module.exports = withCSS(
+//   withLess(
+//     withImages(
+//       withSass({
+//         env: {
+//           ANY_ENV_KEY: 'ANY_ENV_VARIABLE',
+//         },
+//       })
+//     )
+//   )
+// )
+const withImages = require('next-images')
+module.exports = withImages()
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+// import { ChakraProvider } from "@chakra-ui/react"
+import '../public/fonts/globals.css'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // <ChakraProvider>
+    <Component {...pageProps} />
+    // </ChakraProvider>
+  )
 }
 
 export default MyApp
